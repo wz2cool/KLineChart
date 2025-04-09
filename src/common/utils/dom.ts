5875b2434b8dba6bd65dca7339e5944d4994aa30
wz2cool <wz2cool@live.cn>
2025-04-09 18:35:54 +0800

    update comment

--- src/common/utils/dom.ts
+++ src/common/utils/dom.ts
@@ -13,10 +13,23 @@
  */
 
 /**
- * Create dom
- * @param tagName
- * @param styles
- * @return {*}
+ * DOM操作工具函数模块
+ * 提供创建和操作DOM元素的实用工具函数
+ */
+
+/**
+ * 创建DOM元素并设置其样式
+ * @template K 泛型参数，限定为HTMLElementTagNameMap的键类型，用于约束标签名称
+ * @param {K} tagName HTML标签名称，如'div'、'span'等
+ * @param {Partial<CSSStyleDeclaration>} [styles] 可选的样式对象，包含要应用的CSS样式属性
+ * @returns {HTMLElementTagNameMap[K]} 返回创建的DOM元素
+ * @example
+ * // 创建一个div元素并设置样式
+ * const div = createDom('div', {
+ *   width: '100px',
+ *   height: '100px',
+ *   backgroundColor: 'red'
+ * })
  */
 export function createDom<K extends keyof HTMLElementTagNameMap> (tagName: K, styles?: Partial<CSSStyleDeclaration>): HTMLElementTagNameMap[K] {
   const dom = document.createElement(tagName)

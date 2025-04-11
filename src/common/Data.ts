2776657d99707e093200a9edce399e6b6938599b
wz2cool <wz2cool@live.cn>
2025-04-11 17:31:29 +0800

    docs(Data.ts): add JSDoc comments to KLineData and VisibleRangeData interfaces
    Add detailed JSDoc comments to the KLineData and VisibleRangeData interfaces to improve code documentation and clarify the purpose of each property.

--- src/common/Data.ts
+++ src/common/Data.ts
@@ -14,19 +14,38 @@
 
 import type Nullable from './Nullable'
 
+/**
+ * K线数据接口
+ * 定义了一个K线蜡烛图所需的基本数据结构
+ */
 export interface KLineData {
+  /** 时间戳 */
   timestamp: number
+  /** 开盘价 */
   open: number
+  /** 最高价 */
   high: number
+  /** 最低价 */
   low: number
+  /** 收盘价 */
   close: number
+  /** 成交量 */
   volume?: number
+  /** 成交额 */
   turnover?: number
+  /** 其他自定义属性 */
   [key: string]: unknown
 }
 
+/**
+ * 可视范围数据接口
+ * 定义了图表可视区域内每个数据点的信息
+ */
 export interface VisibleRangeData {
+  /** 数据在数组中的索引 */
   dataIndex: number
+  /** 在画布上的X轴坐标 */
   x: number
+  /** 对应的K线数据 */
   data: Nullable<KLineData>
 }
